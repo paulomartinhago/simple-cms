3b3a9c7a694f914a8de5fcc025228f010b036494
paulomartinhago <pmartinhago@gmail.com>
2014-01-26 23:21:46 -0200

    Criação da tabela de usuários

---
+++ app/database/migrations/2014_01_26_190237_create_usuarios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('usuarios', function($table){
+			$table->increments('id');
+			$table->string('username', 100);
+			$table->string('email', 100);
+			$table->string('password', 100);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('usuarios');
+	}
+
+}
