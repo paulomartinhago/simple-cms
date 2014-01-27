d2249a9396b5ee4faae7ab056b7cc6699df215d0
paulomartinhago <pmartinhago@gmail.com>
2014-01-26 23:23:29 -0200

    Arquivo de rotas alterado

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,51 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+/**
+ * LOGIN
+ */
+Route::get('login/{status?}', array('before' => 'guest', function($status = null){
+	return View::make('login.login', array('status' => $status));
+}));
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::post('login', function(){
+	$userdata = array(
+		'email' => Input::get('email'),
+		'password' => Input::get('password')
+	);
+
+	if( Auth::attempt($userdata) )
+		return Redirect::to('/');
+	else
+		return Redirect::to('login/erro');
 });
+
+/**
+ * LOGOUT
+ */
+Route::get('logout', function(){
+	Auth::logout();
+	return Redirect::to('login');
+});
+
+/**
+ * DASHBOARD
+ */
+Route::get('/', array(
+	'before' => 'auth',
+	'uses'   => 'DashboardController@dashboard'
+));
+
+/**
+ * NOTÍCIAS
+ */
+/*Route::get('noticias', 'NoticiasController@noticias');
+Route::get('noticias/adicionar', 'NoticiasController@adicionar');*/
+
+/**
+ * USUÁRIOS
+ */
+/*Route::get('usuarios', 'UsuariosController@usuariosView');
+Route::get('usuarios/adicionar', 'UsuariosController@adicionarView');
+Route::get('usuarios/editar/{id}', 'UsuariosController@actionEditar')->where('id', '[0-9]+');
+Route::get('usuarios/excluir/{id}', 'UsuariosController@actionExcluir')->where('id', '[0-9]+');
+Route::post('usuarios/adicionar', 'UsuariosController@actionAdicionar');*/
